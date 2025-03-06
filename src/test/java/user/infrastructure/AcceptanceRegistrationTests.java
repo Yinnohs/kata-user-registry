8ebca8955954e5b94f44459e9af8d9c3768c0a9c
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2025-03-06 18:37:08 +0000

    Test :white_check_mark: (test): added passing test

--- src/test/java/user/infrastructure/AcceptanceRegistrationTests.java
+++ src/test/java/user/infrastructure/AcceptanceRegistrationTests.java
@@ -99,6 +99,23 @@ public class AcceptanceRegistrationTests {
     }
 
     @Test
+    public void should_not_register_when_passed_empty_password(){
+        //given
+        UserRepository repository = new UserInMemoryRepository();
+        RegisterUser registerUseCase = new RegisterUser(repository);
+        GetAllUsers getAllUsersUseCase = new GetAllUsers(repository);
+        UserController  controller = new UserController(getAllUsersUseCase,registerUseCase);
+
+        String correctEmail = "jose@jose.com";
+        String wrongPassword= "";
+        //when
+
+        Executable result = ()-> controller.registerUser(correctEmail, wrongPassword);
+
+        Assertions.assertThrows(IncorrectCredentialsException.class, result);
+    }
+
+    @Test
     public void should_not_register_when_passed_wrong_email(){
         //given
         UserRepository repository = new UserInMemoryRepository();
