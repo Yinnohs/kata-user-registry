4ca73b16f58127c2ff0b52bb144ad7c6488a6428
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2025-03-06 16:11:13 +0000

    Feature :sparkles: (user) implemented user in memory repository for usecases

--- src/main/java/org/example/user/infrastructure/repository/UserInMemoryRepository.java
+++ src/main/java/org/example/user/infrastructure/repository/UserInMemoryRepository.java
@@ -3,16 +3,21 @@ package org.example.user.infrastructure.repository;
 import org.example.user.application.ports.out.UserRepository;
 import org.example.user.domain.entities.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserInMemoryRepository implements UserRepository {
+
+    List<User> users = new ArrayList<>();
+
     @Override
     public User save(User user) {
-        return null;
+        users.add(user);
+        return user;
     }
 
     @Override
     public List<User> findAll() {
-        return List.of();
+        return users;
     }
 }
