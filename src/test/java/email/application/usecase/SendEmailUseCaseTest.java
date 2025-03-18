c8289c7a0418fdc6f04be7e102c65442303383ad
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2025-03-18 19:27:14 +0000

    Test :test_tube: (email): added use case failing test

--- src/test/java/email/application/usecase/SendEmailUseCaseTest.java
+++ src/test/java/email/application/usecase/SendEmailUseCaseTest.java
@@ -1,6 +1,23 @@
 package email.application.usecase;
 
 
+import email.application.usecase.test.adapter.EmailSenderSpyTestAdapter;
+import org.example.email.application.SendEmailUseCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SendEmailUseCaseTest {
 
+    private final EmailSenderSpyTestAdapter emailSender = new EmailSenderSpyTestAdapter();
+    private final SendEmailUseCase sendEmailUseCase = new SendEmailUseCase(emailSender);
+
+
+    @Test
+    public void should_send_email_when_passed_new_user_email(){
+        String testEmail = "jose@jose.com";
+
+        sendEmailUseCase.execute(testEmail);
+
+        Assertions.assertTrue(emailSender.methodHasBeenExecuteXTimesOrMore(1));
+    }
 }
---
+++ src/test/java/email/application/usecase/test/adapter/EmailSenderSpyTestAdapter.java
@@ -0,0 +1,17 @@
+package email.application.usecase.test.adapter;
+
+import org.example.email.application.ports.out.EmailSender;
+
+public class EmailSenderSpyTestAdapter implements EmailSender {
+
+    private long methodExecuteTimes = 0L;
+
+    @Override
+    public void sendEmail(String email) {
+        System.out.println("EMAIL SENDED TO: " + email);
+    }
+
+    public boolean methodHasBeenExecuteXTimesOrMore(long expectedExecutions){
+        return expectedExecutions <= methodExecuteTimes;
+    }
+}
